679ea877f52bc12aeee4000e12b696ba796fdb77
sabrina matos <sabrinamatosalmeida2@gmail.com>
2024-10-17 08:17:43 -0300

    finalizado exercicio 6

--- src/Aula02_TipoDeDados/Exercícios/Exercicio_06.java
+++ src/Aula02_TipoDeDados/Exercícios/Exercicio_06.java
@@ -15,10 +15,20 @@ public class Exercicio_06 {
 
         boolean resposta;
 
-        //resposta= numero1>numero2;
-        //System.out.println(numero1+">"+numero2+"="+resposta);
+        resposta= numero1>numero2;
+        System.out.println(numero1+">"+numero2+"="+resposta);
+        resposta= numero1>=numero2;
+        System.out.println(numero1+">="+numero2+"="+resposta);
+
+
+
+        resposta= numero1<numero2;
+        System.out.println(numero1+"<"+numero2+"="+resposta);
+        resposta= numero1<=numero2;
+        System.out.println(numero1+"<="+numero2+"="+resposta);
 
 
     }
+
 
 }
